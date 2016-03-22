c6a19a2a8a5ad73a1db8b859835ec56db8c7f70f
Ross Penman <ross@pen.mn>
2016-03-22 17:10:08 +0000

    500 if deploy.sh doesn't work

--- server.rb
+++ server.rb
@@ -4,7 +4,7 @@ require "json"
 def received_data(data)
   return unless ref = data["ref"]
   return unless ref == "refs/heads/#{data["repository"]["default_branch"]}"
-  system "#{__dir__}/deploy.sh"
+  raise unless system "#{__dir__}/deploy.sh"
 end
 
 post "/" do

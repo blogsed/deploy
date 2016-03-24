75e183caa8956f93890853e4a1a13c9e5b6c7ce2
Ross Penman <ross@pen.mn>
2016-03-24 12:56:16 +0000

    Unique directory names to prevent race conditions

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ue
 
-rm -rf tmp/blogsed.github.io
-git clone --depth 1 https://github.com/blogsed/blogsed.github.io tmp/blogsed.github.io
-cp .env tmp/blogsed.github.io
-cd tmp/blogsed.github.io
+dir="tmp/$(uuidgen)"
+rm -rf "$dir"
+git clone --depth 1 https://github.com/blogsed/blogsed.github.io "$dir"
+cp .env "$dir"
+cd "$dir"
 git config user.name "The BLOGS Deployment Robot"
 git config user.email "deploy@blogs.org.uk"
 git remote set-url origin git@github.com:blogsed/blogsed.github.io
 _script/update_events.sh
 _script/deploy.sh
+rm -rf "$dir"
